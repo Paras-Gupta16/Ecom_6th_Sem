914305f1ddfa219bdad7dbb535bc2e8385823772
Paras-Gupta16 <paras94904@gmaul.com>
2026-01-21 14:22:01 +0530

    Working on Seller MVP

---
+++ Ecom_Seller/src/main/java/com/example/Ecom_Seller/EcomSellerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ecom_Seller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomSellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomSellerApplication.class, args);
+	}
+
+}
